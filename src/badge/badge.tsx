155ec50eb09e10b14c73cf0f13c6baee16360ac2
chentiyun <15659263357cty>
2022-03-28 14:22:12 +0800

    feat: 添加徽标组件

--- src/badge/badge.tsx
+++ src/badge/badge.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
+import cls from 'classnames'
 import type {NativeProps} from '../utils/native-props'
 
 const CLASS_PREFIX = 'yui-badge'
@@ -12,8 +13,25 @@ export interface BadgeProps extends NativeProps<'--right' | '--top' | '--color'>
   border?: boolean
 }
 export const Badge: React.FC<BadgeProps> = (props) => {
+
+  const isDot = props.content === dot
+  const badgeCls = cls(CLASS_PREFIX,
+    isDot && `${CLASS_PREFIX}-dot`,
+    !!props.children && `${CLASS_PREFIX}-fixed`,
+    props.className
+    )
+  const element = props.content ? <div
+    className={badgeCls}
+    style={{
+      ...props.style,
+      '--color': props.color
+    } as CSSProperties}
+
+
+  >{!isDot && <div className={`${CLASS_PREFIX}-content`}>{props.content}</div>}</div> : null
   return (
-    <div className={CLASS_PREFIX}>
+    <div className={`${CLASS_PREFIX}-wrap`}>
+      {element}
       {props.children}
     </div>
   )
--- src/badge/index.ts
+++ src/badge/index.ts
@@ -1,4 +1,16 @@
+import {Badge as OriginBadge, dot} from './badge'
+import {attachPropertiesToComponent} from '../utils/attach-properties-to-component'
+export type {BadgeProps} from './badge'
 import './style/badge.less'
-import {Badge} from './badge'
-export type{BadgeProps} from './badge'
+
+
+type BadgeProps = typeof OriginBadge & {
+  dot?: typeof dot
+}
+const Badge:BadgeProps = OriginBadge
+Badge.dot = dot
 export default Badge
+// export default attachPropertiesToComponent(OriginBadge, {
+//   dot: dot
+// })
+
